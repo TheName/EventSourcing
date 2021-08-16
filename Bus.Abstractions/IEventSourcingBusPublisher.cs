85692cf683584d7966ae2cfecf8ae04842cd77d4
Dawid Szmidka <dawid.szmidka@gmail.com>
2021-08-16 22:17:25 +0200

    add missing xml docs

--- Bus.Abstractions/IEventSourcingBusPublisher.cs
+++ Bus.Abstractions/IEventSourcingBusPublisher.cs
@@ -5,8 +5,23 @@ using EventSourcing.Abstractions;
 
 namespace EventSourcing.Bus.Abstractions
 {
+    /// <summary>
+    /// Publishes event stream entries to the event sourcing bus.
+    /// </summary>
     public interface IEventSourcingBusPublisher
     {
+        /// <summary>
+        /// Publishes <paramref name="eventStreamEntries"/> to the event sourcing bus.
+        /// </summary>
+        /// <param name="eventStreamEntries">
+        /// The collection of <see cref="EventStreamEntry"/>
+        /// </param>
+        /// <param name="cancellationToken">
+        /// The <see cref="CancellationToken"/>
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>
+        /// </returns>
         Task PublishAsync(IEnumerable<EventStreamEntry> eventStreamEntries, CancellationToken cancellationToken);
     }
 }
