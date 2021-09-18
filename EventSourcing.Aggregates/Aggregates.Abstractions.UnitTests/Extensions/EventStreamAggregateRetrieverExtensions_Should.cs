36118a0baee64fd9c6389cfef2770c5f62fc873a
Dawid Szmidka <dawid.szmidka@gmail.com>
2021-09-18 09:45:10 +0200

    Add IEventStreamAggregateRetriever generic extensions

---
+++ EventSourcing.Aggregates/Aggregates.Abstractions.UnitTests/Extensions/EventStreamAggregateRetrieverExtensions_Should.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EventSourcing.Abstractions.ValueObjects;
+using EventSourcing.Aggregates.Abstractions.Extensions;
+using EventSourcing.Aggregates.Abstractions.Retrievers;
+using Moq;
+using TestHelpers.Attributes;
+using Xunit;
+
+namespace Aggregates.Abstractions.UnitTests.Extensions
+{
+    public class EventStreamAggregateRetrieverExtensions_Should
+    {
+        [Theory]
+        [AutoMoqData]
+        public async Task Throw_When_CallingRetrieveAsync_And_PassingNullAsAggregateRetriever(EventStreamId streamId)
+        {
+            IEventStreamAggregateRetriever aggregateRetriever = null;
+            await Assert.ThrowsAsync<ArgumentNullException>(() => aggregateRetriever.RetrieveAsync<TestAggregate>(streamId, CancellationToken.None));
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task CastReturnedObjectToProvidedType_When_CallingRetrieveAsync(
+            Mock<IEventStreamAggregateRetriever> aggregateRetrieverMock,
+            EventStreamId eventStreamId)
+        {
+            var aggregate = new TestAggregate();
+            aggregateRetrieverMock
+                .Setup(retriever => retriever.RetrieveAsync(typeof(TestAggregate), eventStreamId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(aggregate);
+
+            var aggregateRetriever = aggregateRetrieverMock.Object;
+
+            var result = await aggregateRetriever.RetrieveAsync<TestAggregate>(eventStreamId, CancellationToken.None);
+            
+            Assert.Equal(aggregate, result);
+        }
+        
+        private class TestAggregate
+        {
+        }
+    }
+}
---
+++ EventSourcing.Aggregates/Aggregates.Abstractions/Extensions/EventStreamAggregateRetrieverExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EventSourcing.Abstractions.ValueObjects;
+using EventSourcing.Aggregates.Abstractions.Retrievers;
+
+namespace EventSourcing.Aggregates.Abstractions.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="IEventStreamAggregateRetriever"/>
+    /// </summary>
+    public static class EventStreamAggregateRetrieverExtensions
+    {
+        /// <summary>
+        /// Retrieves aggregate of type <typeparamref name="T"/> and <paramref name="streamId"/>.
+        /// </summary>
+        /// <param name="aggregateRetriever">
+        /// The <see cref="IEventStreamAggregateRetriever"/>.
+        /// </param>
+        /// <param name="streamId">
+        /// The <see cref="EventStreamId"/>.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// The <see cref="CancellationToken"/>.
+        /// </param>
+        /// <typeparam name="T">
+        /// The <see cref="Type"/> of aggregate.
+        /// </typeparam>
+        /// <returns>
+        /// An instance of <typeparamref name="T"/> with populated event stream with id <paramref name="streamId"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="aggregateRetriever"/> is null.
+        /// </exception>
+        public static async Task<T> RetrieveAsync<T>(
+            this IEventStreamAggregateRetriever aggregateRetriever,
+            EventStreamId streamId,
+            CancellationToken cancellationToken)
+        {
+            if (aggregateRetriever == null)
+            {
+                throw new ArgumentNullException(nameof(aggregateRetriever));
+            }
+
+            return (T) await aggregateRetriever.RetrieveAsync(typeof(T), streamId, cancellationToken)
+                .ConfigureAwait(false);
+        }
+    }
+}
