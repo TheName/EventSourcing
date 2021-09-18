6d5fa578cb47e9f6e1274ffca6bd5b87f1e23645
Dawid Szmidka <dawid.szmidka@gmail.com>
2021-09-18 09:35:37 +0200

    Add aggregate publisher

---
+++ EventSourcing.Aggregates/Aggregates.Abstractions/Publishers/IEventStreamAggregatePublisher.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventSourcing.Aggregates.Abstractions.Publishers
+{
+    /// <summary>
+    /// Publishes event stream aggregates.
+    /// </summary>
+    public interface IEventStreamAggregatePublisher
+    {
+        /// <summary>
+        /// Publishes provided aggregate.
+        /// </summary>
+        /// <param name="aggregate">
+        /// An event stream aggregate.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// The <see cref="CancellationToken"/>.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the action.
+        /// </returns>
+        Task PublishAsync(object aggregate, CancellationToken cancellationToken);
+    }
+}
---
+++ EventSourcing.Aggregates/Aggregates/Publishers/EventStreamAggregatePublisher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EventSourcing.Abstractions;
+using EventSourcing.Aggregates.Abstractions.Conversion;
+using EventSourcing.Aggregates.Abstractions.Publishers;
+
+namespace EventSourcing.Aggregates.Publishers
+{
+    internal class EventStreamAggregatePublisher : IEventStreamAggregatePublisher
+    {
+        private readonly IEventSourcingAggregateConverter _aggregateConverter;
+        private readonly IEventStreamPublisher _eventStreamPublisher;
+
+        public EventStreamAggregatePublisher(
+            IEventSourcingAggregateConverter aggregateConverter,
+            IEventStreamPublisher eventStreamPublisher)
+        {
+            _aggregateConverter = aggregateConverter ?? throw new ArgumentNullException(nameof(aggregateConverter));
+            _eventStreamPublisher = eventStreamPublisher ?? throw new ArgumentNullException(nameof(eventStreamPublisher));
+        }
+        
+        public async Task PublishAsync(object aggregate, CancellationToken cancellationToken)
+        {
+            var publishableStream = _aggregateConverter.ToPublishableEventStream(aggregate);
+
+            await _eventStreamPublisher.PublishAsync(publishableStream, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
