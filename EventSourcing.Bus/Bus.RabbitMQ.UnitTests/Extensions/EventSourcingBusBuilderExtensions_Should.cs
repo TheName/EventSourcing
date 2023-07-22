3a8b1ef912c1bc6c33cd08f911a2a6c67ec96f1b
Dawid Szmidka <dawid.szmidka@gmail.com>
2023-07-22 12:58:11 +0200

    move Bus.RabbitMQ DI code into main EventSourcing.Bus.RabbitMQ package

---
+++ EventSourcing.Bus/Bus.RabbitMQ.UnitTests/Extensions/EventSourcingBusBuilderExtensions_Should.cs
@@ -0,0 +1,73 @@
+﻿using EventSourcing.Bus.Abstractions;
+using EventSourcing.Bus.Extensions;
+using EventSourcing.Bus.RabbitMQ.Extensions;
+using EventSourcing.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using TestHelpers.Extensions;
+using Xunit;
+
+namespace Bus.RabbitMQ.UnitTests.Extensions
+{
+    public class EventSourcingBusBuilderExtensions_Should
+    {
+        [Fact]
+        public void MakeServiceCollectionEligibleToBuildServiceProviderWithoutErrors_When_AddingEventSourcingWithBusUsingRabbitMQAndExternalDependencies_And_UsingDefaults()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection
+                .AddEventSourcing()
+                .WithBus()
+                .UsingRabbitMQ();
+            
+            // External dependencies
+            serviceCollection
+                .AddEventSourcingSerializationMocks()
+                .AddEventSourcingPersistenceMocks()
+                .AddMicrosoftLoggerMock();
+
+            serviceCollection.BuildServiceProvider(
+                new ServiceProviderOptions { ValidateScopes = true, ValidateOnBuild = true });
+        }
+        
+        [Fact]
+        public void MakeServiceCollectionEligibleToBuildServiceProviderWithoutErrors_When_AddingEventSourcingWithBusUsingRabbitMQAndExternalDependencies_And_OptionsWithConsumer()
+        {
+            var serviceCollection = new ServiceCollection();
+                
+            serviceCollection
+                .AddEventSourcing()
+                .WithBus(new EventSourcingBusBuilderOptions(true))
+                .UsingRabbitMQ();
+            
+            // External dependencies
+            serviceCollection
+                .AddEventSourcingSerializationMocks()
+                .AddEventSourcingPersistenceMocks()
+                .AddMicrosoftLoggerMock();
+
+            serviceCollection.BuildServiceProvider(
+                new ServiceProviderOptions { ValidateScopes = true, ValidateOnBuild = true });
+        }
+        
+        [Fact]
+        public void MakeServiceCollectionEligibleToBuildServiceProviderWithoutErrors_When_AddingEventSourcingWithBusUsingRabbitMQAndExternalDependencies_And_OptionsWithoutConsumer()
+        {
+            var serviceCollection = new ServiceCollection();
+                
+            serviceCollection
+                .AddEventSourcing()
+                .WithBus(new EventSourcingBusBuilderOptions(false))
+                .UsingRabbitMQ();
+            
+            // External dependencies
+            serviceCollection
+                .AddEventSourcingSerializationMocks()
+                .AddEventSourcingPersistenceMocks()
+                .AddMicrosoftLoggerMock();
+
+            serviceCollection.BuildServiceProvider(
+                new ServiceProviderOptions { ValidateScopes = true, ValidateOnBuild = true });
+        }
+    }
+}
